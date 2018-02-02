4352bc63627ef46c5852dac7cc7c581249ad64e0
Ijaz <ijaz716@hotmail.com>
2018-02-03 05:17:20 +0800

    Static Files

--- huachain/urls.py
+++ huachain/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('collection/', views.collection, name='collection'),
     path('user_login/', views.user_login, name='user_login'),
     path('user_reg/', views.user_reg, name='user_reg'),
+    path('artwork_reg/', views.artwork_reg, name='artwork_reg'),
     path('log_out/', views.log_out, name='log_out'),
     path('', views.index, name='index'),
 ]
